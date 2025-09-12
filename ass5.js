64e15f1d79d68c1ec3480d24912531a27a1bac58
Ademuyiwa Obaloluwa <obaobasky7@gmail.com>
2025-09-12 15:04:51 -0700

    first commit

--- ass5.js
+++ ass5.js
@@ -8,9 +8,11 @@ switch (true) {
     case isNaN(age) || age <= 0:
         alert("Didn't Get Your Age, Please Reload")
         break;
+
     case age >= 1 && age <= 17:
         alert(`You Are ${age} Years Old, You Are Not Eligible To Drive`)
         break;
+        
     default:
         alert(`You Are ${age} Years Old, You Are Eligible To Drive`)
         break;
