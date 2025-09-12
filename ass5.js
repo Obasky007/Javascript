5766ba76713629f7bdcdc14af1f1bf2e2360298e
Ademuyiwa Obaloluwa <obaobasky7@gmail.com>
2025-09-12 14:15:20 -0700

    first commit

--- ass5.js
+++ ass5.js
@@ -1,1 +1,17 @@
-let 
+let age = prompt("How Old Are You ?")
+
+age = age.trim()
+
+age = Number(age)
+
+switch (true) {
+    case isNaN(age) || age <= 0:
+        alert("Didn't Get Your Age, Please Reload")
+        break;
+    case age >= 1 && age <= 17:
+        alert(`You Are ${age} Years Old, You Are Not Eligible To Drive`)
+        break;
+    default:
+        alert(`You Are ${age} Years Old, You Are Eligible To Drive`)
+        break;
+}   
