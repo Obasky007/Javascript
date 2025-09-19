34ebb4e71b146ea50fcbe1ee2a2feffb67fbce3e
Ademuyiwa Obaloluwa <obaobasky7@gmail.com>
2025-09-19 12:02:13 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+console.log(document.querySelector('p'));
+
+console.log(document.querySelector('#p1'))
+console.log(document.querySelector('#p2'))
+console.log(document.querySelector('#p3'))
+console.log(document.querySelector('#p4'))
+
+console.log(document.querySelectorAll('p'))
